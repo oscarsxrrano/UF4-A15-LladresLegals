bfa3c286cd12c2766036be36f8edf3a665df184e
oscarsxrrano <oscar.serrano.7e5@itb.cat>
2024-02-16 08:41:14 +0100

    Create Project

---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+rootProject.name = "UF4-A15-LladresLegals"
+
---
+++ src/main/kotlin/controllers/Main.kt
@@ -0,0 +1,5 @@
+package controllers
+
+fun main() {
+    println("Hello World!")
+}
